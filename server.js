52f68cd863c779bc61de6d6e2641aa620e0aa1a1
Wompa-Stompa <103228817+Wompa-Stompa@users.noreply.github.com>
2022-07-28 19:02:20 -0500

    Add files via upload

--- server.js
+++ server.js
@@ -1,47 +1,39 @@
-import express from 'express';
-import { engine } from 'express-handlebars';
-
-const exphbs = require('express-handlebars');
-
-// Connection with the helpers, creat carpet utils to add 'helpers.js' file
-const hbs = exphbs.create({});
-
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
-
 const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
-const sequelize = require('./config/connection');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const sequelize = require("./config/connection");
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const sess = {
-    secret: 'Secret',
-    cookie: {},
-    rease: false,
-    saveUnitialized: true,
-    store: new SequelizeStore({
-        db: sequilze,
-    }),
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
 };
 
 app.use(session(sess));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Create carptet 'controllers' to add the routes files
-app.use(routes);
+app.use(require('./controllers/'));
 
-sequilze.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'));
-})
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
